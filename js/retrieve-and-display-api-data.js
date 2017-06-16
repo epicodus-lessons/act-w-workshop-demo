2cd6fbb22a8ba3a1b18dcc76434966c574cbdcac
Courtney Phillips <courtney@epicodus.com>
2017-06-16 11:06:59 -0700

    Call Act-W API, parse response, display on page.

--- js/retrieve-and-display-api-data.js
+++ js/retrieve-and-display-api-data.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
   $('#get-attendees-button').click(function() {
-
-
-  // additional code will go here!
-
-
+    $.get('http://138.197.214.133/api/v1/attendee', function(response) {
+      response.forEach(function(attendeeInformation){
+        $('#list-of-attendees').append(
+         '<div class="well attendee-entry">' +
+           '<b>Age Range: </b>' + attendeeInformation.age_range + '<br>' +
+           '<b>Job Category: </b>' + attendeeInformation.job_category + '<br>' +
+           '<b>Gender: </b>' + attendeeInformation.gender + '<br>' +
+           '<b>Ethnicity: </b>' + attendeeInformation.ethnicity + '<br>' +
+         '</div>'
+       );
+      })
+    });
   });
 });
